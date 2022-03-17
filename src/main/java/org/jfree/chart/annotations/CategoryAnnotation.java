457dc6b48de4e04b9546a716345a446e67142fb0
kxl8596 <101781140+kxl8596@users.noreply.github.com>
2022-03-17 11:58:51 -0400

    Update CategoryAnnotation.java
    HW commit 5

--- src/main/java/org/jfree/chart/annotations/CategoryAnnotation.java
+++ src/main/java/org/jfree/chart/annotations/CategoryAnnotation.java
@@ -61,6 +61,7 @@ public interface CategoryAnnotation extends Annotation {
      * @param domainAxis  the domain axis.
      * @param rangeAxis  the range axis.
      */
+    //TODO Fix Short Variable
     void draw(Graphics2D g2, CategoryPlot plot, Rectangle2D dataArea,
                      CategoryAxis domainAxis, ValueAxis rangeAxis);
 
